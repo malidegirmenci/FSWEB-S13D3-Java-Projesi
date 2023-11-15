bc4f27b6c27b05618b81390f045b4f1de0f184f0
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-15 13:17:17 +0300

    created and imported Wall class

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,16 @@ public class Main {
                 System.out.println(person.getFirstName() + " " + person.getLastName() + " is " + person.getAge() + " year(s) old. So it is not a teen");
             }
          */
+
+        /*Wall Class
+            Wall wall = new Wall(5,4);
+            System.out.println("Wall's width: "+wall.getWidth());
+            System.out.println("Wall's height: "+wall.getHeight());
+            System.out.println("Wall's area: "+wall.getArea());
+            wall.setHeight(-1.5);
+            System.out.println("Wall's height: "+wall.getHeight());
+            System.out.println("Wall's area: "+wall.getArea());
+         */
+
     }
 }
